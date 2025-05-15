8b0d044f6e9a33377a775b6f52ed9a2edf392aa3
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2025-05-15 16:23:13 +0500

    abccc

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,6 @@
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
- console.log(fetchCustomers, 'kkk')
 export default async function Page() {
   const customers = await fetchCustomers();
   return (
