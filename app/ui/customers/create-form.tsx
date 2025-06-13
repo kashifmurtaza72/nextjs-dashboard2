071e9f94307618a045b582c115463f06c5a94cb1
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2025-06-13 16:09:08 +0500

    debuging

--- app/ui/customers/create-form.tsx
+++ app/ui/customers/create-form.tsx
@@ -6,64 +6,22 @@ import { createCustomer, CustomerState } from "@/app/lib/actions";
 import { useActionState, useEffect, useRef } from "react";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
-  const initialState: CustomerState = {message: null, errors: {}, cFieldValues: {}, };
+  const initialState: CustomerState = {
+    message: null,
+    errors: {},
+    cFieldValues: {},
+  };
   const [state, cformAction] = useActionState(createCustomer, initialState);
-  const formRef = useRef<HTMLFormElement>(null);
-
-  useEffect(() => {
-    if (formRef.current) {
-      const customerSelect = formRef.current.elements.namedItem(
-        "customerId"
-      ) as HTMLSelectElement;
-      // if (customerSelect && state.cFieldValues?.customerId) {
-      //   customerSelect.value = state.cFieldValues.customerId;
-      // }
-    }
-  }, [state.cFieldValues]);
-
-  // function handleChange(
-  //   event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  // ) {
-  //   const { name, value } = event.target;
-  //   setFormData((prev) => ({ ...prev, [name]: value }));
-  //   console.log({ ...formData, [name]: value });
-  // }
+  //const formRef = useRef<HTMLFormElement>(null);
 
   return (
-    <form action={cformAction} ref={formRef}>
+    <form action={cformAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Add customer
           </label>
-          {/* <div className="relative">
-            <select
-              id="customer"
-              name="customerId"
-              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue=""
-              aria-describedby="customer-error"
-            >
-              <option value="" disabled>
-                Select a customer
-              </option>
-              {customers.map((name) => (
-                <option key={name.id} value={name.id}>
-                  {name.name}
-                </option>
-              ))}
-            </select>
-            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
-          </div> 
-          <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.customerId &&
-              state.errors.customerId.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
-          </div>*/}
         </div>
 
         {/* Customer name */}
@@ -78,7 +36,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 name="name"
                 type="text"
                 lang="en-US"
-                defaultValue={state.cFieldValues?.name || ""}
+                
                 placeholder="Enter Customer Name"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="name-error"
@@ -107,7 +65,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 name="email"
                 type="text"
                 lang="en-US"
-                defaultValue={state.cFieldValues?.email || ""}
+                
                 placeholder="Enter Customer Email"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="email-error"
@@ -136,7 +94,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 name="image_url"
                 type="text"
                 lang="en-US"
-                defaultValue={state.cFieldValues?.image_url || ""}
+                
                 placeholder="Enter Customer image"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="image_url-error"
@@ -153,58 +111,6 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           </div>
         </div>
 
-        {/* Invoice Status */}
-        {/* <fieldset>
-          <legend className="mb-2 block text-sm font-medium">
-            Set the invoice status
-          </legend>
-          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-            <div className="flex gap-4">
-              <div className="flex items-center">
-                <input
-                  id="pending"
-                  name="status"
-                  defaultChecked={state.cFieldValues?.status === "pending"}
-                  type="radio"
-                  value="pending"
-                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                  aria-describedby="status-error"
-                />
-                <label
-                  htmlFor="pending"
-                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
-                >
-                  Pending <ClockIcon className="h-4 w-4" />
-                </label>
-              </div>
-              <div className="flex items-center">
-                <input
-                  id="paid"
-                  name="status"
-                  defaultChecked={state.cFieldValues?.status === "paid"}
-                  type="radio"
-                  value="paid"
-                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                  aria-describedby="status-error"
-                />
-                <label
-                  htmlFor="paid"
-                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
-                >
-                  Paid <CheckIcon className="h-4 w-4" />
-                </label>
-              </div>
-            </div>
-            <div id="status-error" aria-live="polite" aria-atomic="true">
-              {state.errors?.status &&
-                state.errors.status.map((error: string) => (
-                  <p className="mt-2 text-sm text-red-500" key={error}>
-                    {error}
-                  </p>
-                ))}
-            </div>
-          </div>
-        </fieldset> */}
         {state.errors && (
           <p className="mt-2 text-sm text-red-500">{state.message}</p>
         )}
